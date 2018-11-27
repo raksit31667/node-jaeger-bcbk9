957970ba8623fd052e74da2fc2a6d6f050bda287
raksit <rak-sit@hotmail.com>
2018-11-27 22:44:24 +0700

    :boom: Integrated opencensus Jaeger to expose tracing

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+const tracing = require('@opencensus/nodejs')
+const propagation = require('@opencensus/propagation-b3')
+const jaeger = require('@opencensus/exporter-jaeger')
+const b3 = new propagation.B3Format()
+
+const jaegerOptions = {
+  serviceName: 'node-jaeger-bcbk9',
+  host: 'localhost',
+  port: 6832,
+}
+
+tracing.start({
+    propagation: b3,
+    exporter: new jaeger.JaegerTraceExporter(jaegerOptions)
+})
+
 const express = require('express')
 const axios = require('axios')
-
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
     axios.get('https://httpbin.org/delay/2')
-    .then(axios.get('https://httpbin.org/uuid')
-            .then(response => res.send(response.data)));
-});
+    .then(axios.get('https://httpbin.org/anything')
+            .then(response => res.json(response.data)))
+})
 
 app.listen(3000, () => {
     console.log('Starting Node.js server at port 3000')
